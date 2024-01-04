7905aadefa85d0c78f8fa370a7cc8acd7ef17cfe
benatmencia <bm@divergent-investments.com>
2024-01-04 14:19:47 +0400

    Edit chatbot_physicist.py
    Changes:
    - Edit docs of define_tools

--- chatbot_physicist.py
+++ chatbot_physicist.py
@@ -17,7 +17,9 @@ def load_environment():
     load_dotenv(find_dotenv())
 
 def define_tools():
-    """Define and return a list of tools."""
+    """Return list of tools. The tools are:
+    PythonREPLTool(): used when we need to write and execute python code.
+    """
     return [PythonREPLTool()]
 
 def define_llm_with_tools(tools):
@@ -32,12 +34,11 @@ def create_prompt():
     return ChatPromptTemplate.from_messages(
         [
             ("system", """You are an agent designed to write and execute python code to answer questions.
-You have access to a python REPL, which you can use to execute python code.
-If you get an error, debug your code and try again.
-Only use the output of your code to answer the question. 
-You might know the answer without running any code, but you should still run the code to get the answer.
-If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
-"""),
+                        You have access to a python REPL, which you can use to execute python code.
+                        If you get an error, debug your code and try again.
+                        Only use the output of your code to answer the question. 
+                        You might know the answer without running any code, but you should still run the code to get the answer.
+                        If it does not seem like you can write code to answer the question, just return "I don't know" as the answer."""),
             MessagesPlaceholder(variable_name=MEMORY_KEY),
             ("user", "{input}"),
             MessagesPlaceholder(variable_name="agent_scratchpad"),

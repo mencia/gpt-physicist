d52722878e146b00c5605be143dde2422f203abf
benatmencia <bm@divergent-investments.com>
2024-01-05 10:56:36 +0400

    Edit chatbot_physicist.py
    Changes:
    - Import prompts instead of defining in the script

--- chatbot_physicist.py
+++ chatbot_physicist.py
@@ -11,6 +11,7 @@ from langchain.schema.messages import AIMessage, HumanMessage
 
 from tools_definitions import symbolic_math
 from langchain_experimental.tools import PythonREPLTool
+from prompt_definitions import PROMPT1, PROMPT2
 
 def load_environment():
     """Load environment variables from .env file."""
@@ -102,21 +103,7 @@ def run_chatbot(agent_executor):
 
 if __name__ == "__main__":
 
-    prompt1 = """You are an agent designed to write and execute python code to answer questions.
-    You have access to a python REPL, which you can use to execute python code.
-    If you get an error, debug your code and try again.
-    Only use the output of your code to answer the question. 
-    You might know the answer without running any code, but you should still run the code to get the answer.
-    If it does not seem like you can write code to answer the question, just return "I don't know" as the answer."""
-
-    prompt2 = """You are an agent that has access to two different tools: 1) PythonREPLTool() and 2) symbolic_math. The user will
-    specify which one to use, if the user wants to run a calculation. If the user wants to calculate something and they do not
-    specify which of the two tools to use, ask them. Do not perform calculations until the user has specified what tool to use.
-     When using PythonREPLTool(): a) If you get an error, debug your code and try again, b) only use the output of your code to answer the question and c) if it does not seem like you can write code
-    to answer the question, just return "I don't know" as the answer. When using symbolic_math, use the result to
-    respond to the user."""
-
-    prompt = prompt2
+    prompt = PROMPT2
 
     load_environment()
     tools = define_tools()
